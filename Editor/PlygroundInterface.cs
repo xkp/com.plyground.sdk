c1835e8cb1ed7d48993f90b6abf8467c32a65aba
xkp <xkp.santos@gmail.com>
2026-09-02 13:49:13 -0400

    adding newtonsoft

--- Editor/PlygroundInterface.cs
+++ Editor/PlygroundInterface.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Unity.Plastic.Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
